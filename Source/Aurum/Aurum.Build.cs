8707ce5bb1d6f188b88fcea36a7251717fda5e3e
Andrii Shelengovkyi <thesheleng@gmail.com>
2026-05-30 11:45:52 +0200

    Install Plugins: Mover, GameplayAbilities and NinjaInput

--- Source/Aurum/Aurum.Build.cs
+++ Source/Aurum/Aurum.Build.cs
@@ -8,7 +8,19 @@ public class Aurum : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput",
+			"NinjaInput",
+			"Mover",
+			"GameplayAbilities",
+			"GameplayTasks",
+			"GameplayTags",
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
